692cd010175af9513fc147a45e17bd75e18af20c
Keerthan K V <keerthan11c@gmail.com>
2025-11-25 01:42:38 +0530

    Add parallel Fibonacci function using OpenMP
    Implement parallel Fibonacci calculation using OpenMP.

---
+++ fib.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <omp.h>
+
+int fib(int n) {
+    int x, y;
+
+    if (n < 2)
+        return n;
+
+    #pragma omp task shared(x)
+    x = fib(n - 1);
+
+    #pragma omp task shared(y)
+    y = fib(n - 2);
+
+    #pragma omp taskwait
+
+    return x + y;
+}
+
+int main() {
+    int n = 10, result;
+
+    #pragma omp parallel
+    {
+        #pragma omp single
+        result = fib(n);
+    }
+
+    printf("Fibonacci(%d) = %d\n", n, result);
+    return 0;
+}
+
+
+
+// gcc pg6.c -o pg6 -fopenmp
+// ./pg6
+
+// Fibonacci(10) = 55
